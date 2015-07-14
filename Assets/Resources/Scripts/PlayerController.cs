b7f01ec33726fb92030e248f92ed46fe1d168742
Klemen Brajkovič <d3cr1pt0r@gmail.com>
2015-07-14 02:35:54 +0200

    - added custom player jumping and collision detection

--- Assets/Resources/Scripts/PlayerController.cs
+++ Assets/Resources/Scripts/PlayerController.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+	public float runSpeed;
+	public float jumpSpeed;
+
+	private bool isJumping = true;
+	private int collisionNum = 0;
 
 	void Start ()
 	{
@@ -11,10 +16,29 @@ public class PlayerController : MonoBehaviour
 
 	void Update ()
 	{
-		if (Input.GetKeyDown (KeyCode.Space))
+		if (!isJumping && Input.GetKeyDown (KeyCode.Space))
 		{
-			gameObject.GetComponent<Rigidbody2D>().gravityScale *= -1;
 			gameObject.transform.Rotate(0, 180, 180);
+			jumpSpeed *= -1;
+			isJumping = true;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (isJumping) {
+			gameObject.transform.position -= new Vector3(0, jumpSpeed, 0);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		if (isJumping && collider.tag == "Floor") {
+			collisionNum++;
+			isJumping = false;
+
+			Debug.Log (collisionNum);
+			Debug.Log (isJumping);
 		}
 	}
 }
