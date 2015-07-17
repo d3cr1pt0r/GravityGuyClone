50740b6c2df855e386972e6df2b7483b58ecaaa9
Klemen Brajkovič <klemen.brajkovic@gmail.com>
2015-07-18 01:35:43 +0200

    - debug messages update

--- Assets/Resources/Scripts/NetworkController.cs
+++ Assets/Resources/Scripts/NetworkController.cs
@@ -6,43 +6,6 @@ using System.Collections;
 using System.Threading;
 using System.IO;
 
-public class CoroutineExample : MonoBehaviour {
-	public static NetworkEventType serverEvent;
-
-	public static IEnumerator CoroutineReceiveRequest() {
-		int recHostId;
-		int connectionId;
-		int channelId;
-		int dataSize;
-		byte[] buffer = new byte[1024];
-		byte error;
-
-		while (true) {
-			NetworkEventType recData = NetworkTransport.Receive (out recHostId, out connectionId, out channelId, buffer, 1024, out dataSize, out error);
-			switch(recData) {
-				case NetworkEventType.Nothing:
-					break;
-				case NetworkEventType.ConnectEvent:
-					Debug.Log ("Connection received...");
-					break;
-				case NetworkEventType.DisconnectEvent:
-					Debug.Log ("DisconnectEvent");
-					break;
-				case NetworkEventType.DataEvent:
-					string data = System.Text.Encoding.ASCII.GetString (buffer);
-					Debug.Log ("DataEvent: " + data);
-					break;
-				case NetworkEventType.BroadcastEvent:
-					Debug.Log ("BroadcastEvent");
-					break;
-				default:
-					break;
-			}
-			yield return null;
-		}
-	}
-}
-
 public class NetworkController : MonoBehaviour {
 
 	public InputField inputFieldAddress;
@@ -55,11 +18,10 @@ public class NetworkController : MonoBehaviour {
 		inputFieldAddress.text = "192.168.0.100";
 
 		peer = new Peer(PORT);
-		peer.onConnected += new Peer.NetworkEvent(onPeerConnected); 
+		peer.onConnected += new Peer.NetworkEvent(onPeerConnected);
+
 		Debug.Log (peer.socketAlive);
-
 	}
-
 
 	void Update() {
 
@@ -69,12 +31,12 @@ public class NetworkController : MonoBehaviour {
 	}
 
 	void onPeerConnected(Peer p, int connectionId) {
-		Debug.Log (connectionId);
+		Debug.Log ("Peer connected: " + connectionId.ToString());
 	}
 
 	public void OnButtonConnectClick() {
 		if (peer.connectSocket(inputFieldAddress.text, PORT)) {
-			Debug.Log("connected");
+			Debug.Log("Connected to peer");
 		}
 	}
 
